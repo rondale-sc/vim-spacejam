5bf2a75a127e8f192c1396f1ac0d48c926f14feb
Jake Worth <jake@jakeworth.com>
2016-09-06 12:13:17 -0500

    Add Elixir as a default file type

--- spec/plugin/spacejam_spec.rb
+++ spec/plugin/spacejam_spec.rb
@@ -12,7 +12,7 @@ shared_context "strips trailing whitespace" do
 end
 
 describe "spacejam.vim" do
-  let(:default_filetypes) { 'ruby,javascript,vim,perl,sass,scss,css,coffee,haml' }
+  let(:default_filetypes) { 'ruby,javascript,vim,perl,sass,scss,css,coffee,haml,elixir' }
   let(:plugin_path) { File.expand_path('../../../',__FILE__) }
 
   context "overriding defaults" do
